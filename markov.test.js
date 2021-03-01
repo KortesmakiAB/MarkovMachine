b45b17b1d263a97b341ab0f76616d735b4a3762f
Aaron Brant <72094000+KortesmakiAB@users.noreply.github.com>
2021-03-01 16:24:46 -0500

    add jest unit tests for makeChains and makeTest methods.

--- markov.test.js
+++ markov.test.js
@@ -1,13 +1,37 @@
 const { MarkovMachine } = require('./markov');
 
-// const mm = new MarkovMachine(`Not in a box.
-// Not with a fox.
-// Not in a house.
-// Not with a mouse.
-// I would not eat them here or there.
-// I would not eat them anywhere.
-// I would not eat green eggs and ham.
-// I do not like them, Sam-I-am.`);
-// mm.makeText()
+describe('makeChains method tests', () => {
 
-console.log(MarkovMachine.getText())
+    test('First word in "words" array should be a key. Second word in same array should be added to values array. Constructor should convert words to lower case and remove periods and exclamation marks.', () => {
+        const mmTest = new MarkovMachine('Hello. World!');
+        expect(mmTest.chains['hello'][0]).toBe('world');
+    });
+
+    test('When last word in array is a key, value array should contain 1 value, null ', () => {
+        const mmTest = new MarkovMachine('You are my sunshine');
+        expect(mmTest.chains['sunshine'].length).toBe(1);
+        expect(mmTest.chains['sunshine'][0]).toBe(null);
+    });
+});
+
+
+describe('makeText method tests', () => {
+    let mmTest;
+    
+    beforeAll(() => {
+        mmTest = new MarkovMachine('Amazing grace how sweet the sound.');
+    });
+
+    test('There should be 10 words. Output should be a string.', () => {
+        const test = mmTest.makeText(10);
+        expect(test.split(' ').length).toBe(10);
+        expect(typeof test).toBe('string');
+    });
+
+    test('Should not contain other words or types.', () => {
+        const test = mmTest.makeText(10);
+        expect(test).not.toContain('TestWord');
+        expect(test).not.toContain(undefined);
+        expect(test).not.toContain(true);
+    });
+});
